ca4298ff523aec17318efcaebec079ba4549eee9
npmrun <kk1549469775@gmail.com>
2020-09-21 16:21:24 +0800

    modify some articles

--- themes/sexy/scripts/event.js
+++ themes/sexy/scripts/event.js
@@ -1,5 +1,13 @@
-hexo.extend.helper.register("test", function () {
+hexo.extend.helper.register("sortPosts", function () {
   return hexo.locals.get('posts').data.sort(function(a, b){
     return a.slug.localeCompare(b.slug)
   });
+});
+hexo.extend.helper.register("filterPosts", function () {
+  let posts = hexo.locals.get('posts').data;
+  let allTag = posts.map(v=>v.slug.split("/").slice(0,-1).join("/"))
+  allTag = allTag.filter(function(item, index, arr) {
+    return allTag.indexOf(item) == index;
+  });
+  return allTag
 });
